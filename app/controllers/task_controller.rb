0834b729d8f52887077ffc9e20b825f9ae19047d
Hikari Yanagihara <Hikari@Hikari-no-MacBook-Pro.local>
2018-04-13 18:18:07 +0900

    post機能作成

--- app/controllers/task_controller.rb
+++ app/controllers/task_controller.rb
@@ -2,4 +2,25 @@ class TaskController < ApplicationController
   def list
     @task = Task.all
   end
+
+  # 作成画面
+  def new
+    @task = Task.new
+  end
+
+  # 投稿
+  def create
+    @task = Task.new(task_params)
+    if @task.save
+      flash[:success] = "success!!"
+      redirect_to @task
+    else
+      render 'new'
+    end
+  end
+
+  private
+  def task_params
+    params.require(:task).permit(:task_name, :term_at)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get 'task/list'
-
+  get 'task/new', to: 'task#new'
+  post 'task/new', to: 'task#create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
