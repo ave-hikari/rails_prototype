3c6a4e3197f532a9c204f2b1915b21be9898e3e2
Hikari Yanagihara <Hikari@Hikari-no-MacBook-Pro.local>
2018-04-13 15:39:33 +0900

    Model修正

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,2 +1,7 @@
 class Task < ApplicationRecord
+  def disp_term_at
+    # 期限日+残り日数を設定
+    term_string = '期限日まであと' + ((Date.parse(term_at.to_s) - Date.today).to_i).to_s + '日'
+    Date.today > Date.parse(term_at.to_s) ? '期限切れ' : term_at.to_s + term_string
+  end
 end
