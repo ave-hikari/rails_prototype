4972fbccd48e3706f4b65d7a87270f72b6e6e805
Hikari Yanagihara <ave.nxson@gmail.com>
2018-04-15 22:39:37 +0900

    git config反映確認

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,6 @@ Rails.application.routes.draw do
   get 'task/new', to: 'task#new'
   post 'task/new', to: 'task#create'
   delete 'task/delete', to: 'task#delete'
-  # TODO: resourcesを指定しない場合でのupdate,deleteでidを渡すことはできないのか？
-  # 
+  #
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
